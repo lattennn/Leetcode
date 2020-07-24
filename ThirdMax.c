dc425f925521d8c0753ab9cabc9ca96996063b0d
lattennn <48510816+lattennn@users.noreply.github.com>
2020-07-25 00:43:42 +0200

    Update ThirdMax.c

--- ThirdMax.c
+++ ThirdMax.c
@@ -1,3 +1,7 @@
+
+//Given a non-empty array of integers, return the third maximum number in this array. 
+//If it does not exist, return the maximum number. The time complexity must be in O(n).
+
 #include<stdlib.h>
 #include<stdio.h>
 
