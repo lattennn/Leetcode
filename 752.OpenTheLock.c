44709ff8e6dad5d7edc10f4055371228c57d5dbd
lattennn <48510816+lattennn@users.noreply.github.com>
2020-08-05 00:43:41 +0200

    Update 752.OpenTheLock.c

--- 752.OpenTheLock.c
+++ 752.OpenTheLock.c
@@ -1,4 +1,6 @@
-// You have a lock in front of you with 4 circular wheels. Each wheel has 10 slots: '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'. The wheels can rotate freely and wrap around: for example we can turn '9' to be '0', or '0' to be '9'. Each move consists of turning one wheel one slot.
+
+// You have a lock in front of you with 4 circular wheels. Each wheel has 10 slots: '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'. 
+//The wheels can rotate freely and wrap around: for example we can turn '9' to be '0', or '0' to be '9'. Each move consists of turning one wheel one slot.
 
 // The lock initially starts at '0000', a string representing the state of the 4 wheels.
 
